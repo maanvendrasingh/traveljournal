fadc76184039b1b15fb24b68d738ddbba2ea5d26
Maanvendra Singh <maansingh2618@gmail.com>
2022-03-29 11:33:24 +0530

    package.json updated

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+export default function Card(props) {
+  return (
+    <section>
+      <div className="section">
+        <div className="section-header">
+          <img src={props.item.imageUrl} className="section-icon" />
+
+          <div className="section-content">
+            <img
+              src="../images/location-icon.png"
+              className="section-nav-icon"
+            />
+            <span className="section-country">{props.item.location}</span>
+            <a target="_blank" href={props.item.googleMapsUrl}>
+              <span className="section-location">View on Google Maps</span>
+            </a>
+
+            <h1>{props.item.title}</h1>
+            <p className="section-date">
+              <b>{props.item.startDate} - {props.item.endDate}</b>
+            </p>
+            <p className="section-desc">
+              {props.item.description}
+            </p>
+          </div>
+        </div>
+        <hr></hr>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <nav className="nav">
+            <img src="../images/journal-icon.png" alt="journal-icon" className="nav-icon"/>
+            <h3>My travel Journal</h3>
+        </nav>
+    )
+}
